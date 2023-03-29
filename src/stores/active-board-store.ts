b1e91e6c826a15439871f672f811adf5ba23e473
Enrique Santos <santosenrique869@gmail.com>
2023-03-29 08:18:14 -0300

    feat: create store to control active board state

---
+++ src/stores/active-board-store.ts
@@ -0,0 +1,25 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+import { BOARD_ACTIVE_KEY } from '~/constants/board-active';
+
+export type BoardType = {
+	id: string;
+	name: string;
+};
+
+type ActiveBoardState = {
+	activeBoard: BoardType | undefined;
+	setActiveBoard: (boardId: BoardType) => void;
+};
+
+export const useActiveBoardStore = create(
+	persist<ActiveBoardState>(
+		(set) => ({
+			activeBoard: undefined,
+			setActiveBoard: (board: BoardType) => set({ activeBoard: board })
+		}),
+		{
+			name: BOARD_ACTIVE_KEY
+		}
+	)
+);
