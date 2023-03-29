733209fa25259649b00e39e66d2e4a8a707457cb
Enrique Santos <santosenrique869@gmail.com>
2023-03-29 08:22:48 -0300

    refactor: change async call to backend to sintaxe then-catch over async-await to simplify code

--- src/shared/loaders/loader-private-routes.ts
+++ src/shared/loaders/loader-private-routes.ts
@@ -1,19 +1,16 @@
 import { redirect } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { getProfile } from '~/services/auth.service';
-import { getAuthToken, removeAuthToken } from '~/utils/auth';
+import { getAuthToken } from '~/utils/auth';
 
-export async function loaderPrivateRoutes() {
+export async function verifyUserIsAuthenticatedLoader() {
 	const token = getAuthToken();
-	if (!token) return redirect('/login');
+	if (!token) return redirect('/auth/login');
 
-	try {
-		await getProfile();
-	} catch (error) {
+	await getProfile().catch(() => {
 		toast.error('Your session has expired, please login again');
-		removeAuthToken();
-		return setTimeout(() => redirect('/login'), 2000);
-	}
+		return setTimeout(() => redirect('/auth/login'), 2000);
+	});
 
 	return null;
 }
