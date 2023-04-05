298e745d4446fc818c1020bec505034f02507720
Enrique Santos <santosenrique869@gmail.com>
2023-04-05 11:25:30 -0300

    refactor: add cursor pointer

--- src/shared/components/Label.tsx
+++ src/shared/components/Label.tsx
@@ -5,7 +5,7 @@ interface LabelTextProps {
 
 export function Label({ label, children }: LabelTextProps) {
 	return (
-		<label className="text-mediumGrey flex flex-col gap-2 text-sm font-bold first-letter:capitalize dark:text-white">
+		<label className="text-mediumGrey flex cursor-pointer flex-col gap-2 text-sm font-bold first-letter:capitalize dark:text-white">
 			{label}
 			{children}
 		</label>
