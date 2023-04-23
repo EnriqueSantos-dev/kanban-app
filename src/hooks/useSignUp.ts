f9ee2f540ca1a4d254ade3c4dc71e9e62a4e5f73
Enrique Santos <santosenrique869@gmail.com>
2023-04-23 15:16:45 -0300

    feat: create hook useSignUpMutation

---
+++ src/hooks/useSignUp.ts
@@ -0,0 +1,24 @@
+import { useMutation } from '@tanstack/react-query';
+import { useNavigate } from 'react-router-dom';
+import { FormValues } from '~/pages/register/components/RegisterForm';
+import { signup } from '~/services/auth.service';
+import { ErrorApi } from '~/types';
+import { useNotificationToasty } from './useNotificationToasty';
+
+export const useSignUpMutation = () => {
+	const notification = useNotificationToasty();
+	const navigate = useNavigate();
+
+	return useMutation<void, ErrorApi, FormValues>({
+		mutationFn: (data) => signup(data),
+		onSuccess: () => {
+			notification('success', 'Register success, your are redirecting...');
+			setTimeout(() => navigate('/auth/login'), 2000);
+		},
+		onError: (error) => {
+			const errorMessage =
+				error.response?.data?.message ?? 'Something went wrong';
+			notification('error', errorMessage);
+		}
+	});
+};
