59dab546e23d85e32fc581c375cf7e9e05a10625
Enrique Santos <santosenrique869@gmail.com>
2023-04-23 15:16:04 -0300

    feat: create hook useSignInMutation

---
+++ src/hooks/useSignIn.ts
@@ -0,0 +1,30 @@
+import { useMutation } from '@tanstack/react-query';
+import { useNavigate } from 'react-router-dom';
+import { ResponseSignIn, SignInRequest, signin } from '~/services/auth.service';
+import { ErrorApi } from '~/types';
+import { setAuthToken } from '~/utils/auth';
+import { useNotificationToasty } from './useNotificationToasty';
+
+export const useSignInMutation = ({
+	callback
+}: {
+	callback: (...args: any) => void;
+}) => {
+	const navigate = useNavigate();
+	const notification = useNotificationToasty();
+
+	return useMutation<ResponseSignIn, ErrorApi, SignInRequest>({
+		mutationFn: (data) => signin(data),
+		onSuccess: (data) => {
+			setAuthToken(data.access_token);
+			callback(data.access_token);
+			notification('success', 'login success, your are redirecting...');
+			setTimeout(() => navigate('/'), 2000);
+		},
+		onError: (error) => {
+			const errorMessage =
+				error.response?.data?.message ?? 'Something went wrong';
+			notification('error', errorMessage);
+		}
+	});
+};
