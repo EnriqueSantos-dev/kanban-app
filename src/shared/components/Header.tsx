10539d24fb1dcced69919c7faadd397fa1ca2249
Enrique Santos <santosenrique869@gmail.com>
2023-04-23 15:22:35 -0300

    feat: use AddNewTaskForm inside component

--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -1,14 +1,15 @@
 import KanbanLogoSvg from '~/assets/logo.svg';
-import { useActiveBoard } from '~/hooks/useActiveBoard';
+import { useActiveBoard } from '~/hooks';
 import {
 	MobileMenuPopover,
-	PopoverEditOrDeleteBoard
+	PopoverEditOrDeleteBoard,
+	AddNewTaskForm
 } from '~/shared/components';
 import { useMenuStore } from '~/stores/menu-store';
 import { cn } from '~/utils/cn';
 
 export function Header() {
-	const { activeBoard } = useActiveBoard();
+	const { activeBoard, setActiveBoard } = useActiveBoard();
 	const { isMenuOpen } = useMenuStore();
 
 	return (
@@ -24,7 +25,6 @@ export function Header() {
 					)}
 				>
 					<KanbanLogoSvg />
-
 					<p className="text-3xl font-bold leading-relaxed text-black dark:text-white">
 						Kanban
 					</p>
@@ -46,32 +46,15 @@ export function Header() {
 						>
 							{activeBoard?.name ?? 'No board selected'}
 						</h2>
-
 						<MobileMenuPopover />
 					</div>
 
 					<div className="flex items-center gap-2">
-						<button
-							type="button"
-							className="bg-purple hover:bg-purpleHover focus:ring-purple dark:focus:ring-offset-darkGrey flex items-center justify-center gap-2 whitespace-nowrap rounded-full px-4 py-3 font-bold text-white transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2"
-						>
-							<svg
-								width="12"
-								height="12"
-								xmlns="http://www.w3.org/2000/svg"
-								className="shrink-0"
-							>
-								<path
-									fill="#fff"
-									d="M7.368 12V7.344H12V4.632H7.368V0H4.656v4.632H0v2.712h4.656V12z"
-								/>
-							</svg>
-
-							<span className="hidden text-sm md:inline-block">
-								Add New Task
-							</span>
-						</button>
-						<PopoverEditOrDeleteBoard />
+						<AddNewTaskForm activeBoard={activeBoard} />
+						<PopoverEditOrDeleteBoard
+							activeBoard={activeBoard}
+							setActiveBoard={setActiveBoard}
+						/>
 					</div>
 				</div>
 			</nav>
