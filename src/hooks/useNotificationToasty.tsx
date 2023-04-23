9cc89e95fedf83126316779754722d62b48f269b
Enrique Santos <santosenrique869@gmail.com>
2023-04-23 15:15:32 -0300

    refactor: pass directly types provide from lib

--- src/hooks/useNotificationToasty.tsx
+++ src/hooks/useNotificationToasty.tsx
@@ -1,17 +1,14 @@
-/* eslint-disable @typescript-eslint/naming-convention */
 import { ToastOptions, TypeOptions, toast } from 'react-toastify';
 
-type NOTIFICATION_TYPES = TypeOptions;
-
 const defaultOptions = {
-	autoClose: 1000,
+	autoClose: 500,
 	delay: 0,
 	type: 'success'
 } satisfies ToastOptions;
 
 export function useNotificationToasty() {
 	const notification = (
-		notificationType: NOTIFICATION_TYPES,
+		notificationType: TypeOptions,
 		message: string,
 		options?: ToastOptions
 	) => toast(message, { ...options, type: notificationType } ?? defaultOptions);
