cdfa1ce0cfce4be18ae603af5e11b9d3eb1f9bbb
Enrique Santos <santosenrique869@gmail.com>
2023-04-23 15:42:13 -0300

    feat: create service to manage boards

---
+++ src/services/boards.service.ts
@@ -0,0 +1,40 @@
+import { isAxiosError } from 'axios';
+import { api } from '~/lib';
+
+export type CreateBoardInput = {
+	name: string;
+	initialColumns: string[];
+};
+
+export async function createBoard(input: CreateBoardInput): Promise<void> {
+	try {
+		await api.post('/boards', input);
+	} catch (error) {
+		if (isAxiosError(error)) {
+			if (error.response?.status === 409) {
+				throw new Error('Board already exists');
+			}
+
+			throw new Error('Something went wrong');
+		}
+	}
+}
+
+export type EditBoardInput = {
+	id: string;
+	name: string;
+	columns: Array<{ id?: string; name: string }>;
+};
+
+export type EditBoardOutput = {
+	name: string;
+	columns: Array<{ id: string; name: string }>;
+};
+
+export async function editBoard(
+	input: EditBoardInput
+): Promise<EditBoardOutput> {
+	const { id, ...rest } = input;
+	const { data } = await api.patch(`/boards/${id}/update`, rest);
+	return data;
+}
