6699b47fa5ee6d674ac0f373c120fb58635c1857
Enrique Santos <santosenrique869@gmail.com>
2023-04-26 17:19:08 -0300

    refactor: use react-hot-toasty on show notification

--- src/shared/loaders/require-auth.ts
+++ src/shared/loaders/require-auth.ts
@@ -1,5 +1,5 @@
 import { redirect } from 'react-router-dom';
-import { toast } from 'react-toastify';
+import { toast } from 'react-hot-toast';
 import { verifyToken } from '~/services/auth.service';
 import { getAuthToken } from '~/utils/auth';
 
