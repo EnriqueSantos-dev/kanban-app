b9ea3d6cf3b10fb807a2aea601190611507d03e0
Enrique Santos <santosenrique869@gmail.com>
2023-04-26 17:32:03 -0300

    chore: remove old import lib

--- src/shared/components/AppProviders.tsx
+++ src/shared/components/AppProviders.tsx
@@ -1,5 +1,3 @@
-import 'react-toastify/dist/ReactToastify.css';
-
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
