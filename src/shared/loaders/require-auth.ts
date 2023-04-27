78d335071f3a7dd9984f046a911b1020f8c83c35
Enrique Santos <santosenrique869@gmail.com>
2023-04-26 23:44:03 -0300

    refactor: try logout user if verify token throws error

--- src/shared/loaders/require-auth.ts
+++ src/shared/loaders/require-auth.ts
@@ -1,17 +1,23 @@
-import { redirect } from 'react-router-dom';
 import { toast } from 'react-hot-toast';
-import { verifyToken } from '~/services/auth.service';
-import { getAuthToken } from '~/utils/auth';
+import { redirect } from 'react-router-dom';
+import { logoutUser, verifyToken } from '~/services/auth.service';
+import { getAuthToken, removeAuthToken } from '~/utils/auth';
 
 export async function requireAuth() {
 	const token = getAuthToken();
 	if (!token) return redirect('/auth/login');
 
 	try {
+		if (!token) return redirect('/auth/login');
 		await verifyToken();
-	} catch (_error) {
+	} catch (e) {
 		toast.error('Your session has expired, please login again');
-		return redirect('/auth/login');
+		await logoutUser()
+			.then(() => redirect('/auth/login'))
+			.catch(() => {
+				removeAuthToken();
+				return redirect('/auth/login');
+			});
 	}
 
 	return null;
