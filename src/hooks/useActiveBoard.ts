21d08036424b21ba51d7a23d3cfd496d4251b272
Enrique Santos <santosenrique869@gmail.com>
2023-04-27 19:28:37 -0300

    refactor: remove verification if board exists

--- src/hooks/useActiveBoard.ts
+++ src/hooks/useActiveBoard.ts
@@ -1,22 +1,7 @@
-import { useEffect } from 'react';
 import { useActiveBoardStore } from '~/stores/active-board-store';
-import { useAuthStore } from '~/stores/auth-store';
 
 export const useActiveBoard = () => {
-	const { user } = useAuthStore();
 	const { activeBoard, setActiveBoard } = useActiveBoardStore();
-
-	useEffect(() => {
-		if (activeBoard) {
-			const existingBoard = user?.boards.find(
-				(board) => board.id === activeBoard.id
-			);
-
-			if (!existingBoard) {
-				setActiveBoard(undefined);
-			}
-		}
-	}, []);
 
 	return { activeBoard, setActiveBoard };
 };
