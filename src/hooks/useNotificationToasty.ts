8f99d6c9207a228264998dfbf39c9df6bf8364c7
Enrique Santos <santosenrique869@gmail.com>
2023-04-26 22:05:41 -0300

    feat: create new handler to working with promises

--- src/hooks/useNotificationToasty.ts
+++ src/hooks/useNotificationToasty.ts
@@ -1,20 +1,53 @@
-import { ToastOptions, toast } from 'react-hot-toast';
+import {
+	Renderable,
+	ToastOptions,
+	ToastType,
+	ValueOrFunction,
+	toast
+} from 'react-hot-toast';
+import { ErrorApi } from '~/types';
 import { useTheme } from './useTheme';
 
 export function useNotificationToasty() {
 	const { theme } = useTheme();
+	const style = {
+		backgroundColor: theme === 'dark' ? '#20212C' : '#fff',
+		color: theme === 'dark' ? '#fff' : '#20212C'
+	};
+
 	const notification = (
-		notificationType: 'success' | 'error',
+		notificationType: Exclude<ToastType, 'blank'>,
 		message: string,
 		options?: ToastOptions
 	) =>
 		toast[notificationType](message, {
 			...options,
-			style: {
-				backgroundColor: theme === 'dark' ? '#20212C' : '#fff',
-				color: theme === 'dark' ? '#fff' : '#20212C'
-			}
+			style
 		});
 
-	return notification;
+	const notificationLoading = <T>(
+		promise: Promise<T>,
+		{
+			loading,
+			success,
+			error
+		}: {
+			loading: Renderable;
+			success: ValueOrFunction<Renderable, T>;
+			error?: ValueOrFunction<Renderable, ErrorApi>;
+		}
+	): Promise<T> =>
+		toast.promise(
+			promise,
+			{
+				loading,
+				success,
+				error: error ?? 'Something went wrong'
+			},
+			{
+				style
+			}
+		);
+
+	return { notification, notificationLoading };
 }
