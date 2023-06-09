c8a6cbf9f2fb43b9bffc541d9856325ffa4fc081
Enrique Santos <santosenrique869@gmail.com>
2023-06-09 10:58:25 -0300

    style: add shadow classe

--- src/shared/components/OverlayTaskCard.tsx
+++ src/shared/components/OverlayTaskCard.tsx
@@ -12,7 +12,7 @@ export function OverlayTaskCard({
 	return (
 		<div
 			aria-label="task card overlay"
-			className="dark:bg-darkGrey dark:shadow-veryDarkGrey focus:ring-purple h-[90px] touch-manipulation rounded-lg border-none bg-white px-4 py-6 shadow-sm outline-none transition duration-200 ease-in-out hover:shadow-md focus:ring-1 focus:ring-offset-1"
+			className="dark:bg-darkGrey shadow-taskCard focus:ring-purple h-[90px] touch-manipulation rounded-lg border-none bg-white px-4 py-6 outline-none transition duration-200 ease-in-out focus:ring-1 focus:ring-offset-1"
 		>
 			<p className="max-w-[150px] text-ellipsis text-base/6 font-bold text-black dark:text-white">
 				{name}
