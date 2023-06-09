2462af0532ef3daaea271b9317a9ec6deeebdf80
Enrique Santos <santosenrique869@gmail.com>
2023-06-08 21:23:39 -0300

    feat: create skeleton task component

---
+++ src/shared/components/SkeletonTask.tsx
@@ -0,0 +1,5 @@
+export function SkeletonTask() {
+	return (
+		<div className="dark:from-darkGrey from-linesLight pointer-events-none min-h-[90px] w-full animate-pulse rounded-lg bg-gradient-to-tr to-slate-300 dark:to-slate-800" />
+	);
+}
