bc736aea4cdfaeae72f0e45e452319ae1804694a
Enrique Santos <santosenrique869@gmail.com>
2023-06-09 09:41:39 -0300

    refactor: invalidate getTasks  query on mutate

--- src/hooks/useCreateNewTask.ts
+++ src/hooks/useCreateNewTask.ts
@@ -1,12 +1,23 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import {
+	createTask,
 	CreateTaskInput,
-	CreateTaskOutPutDto,
-	createTask
+	CreateTaskOutPutDto
 } from '~/services/tasks.service';
+import { useActiveBoardStore } from '~/stores/active-board-store';
 import { ErrorApi } from '~/types';
+import { getTasksKey } from './useGetTasks';
 
-export const useCreateNewTaskMutation = () =>
-	useMutation<CreateTaskOutPutDto, ErrorApi, CreateTaskInput>({
-		mutationFn: createTask
+export const useCreateNewTaskMutation = () => {
+	const queryClient = useQueryClient();
+	const activeBoard = useActiveBoardStore((state) => state.activeBoard);
+
+	return useMutation<CreateTaskOutPutDto, ErrorApi, CreateTaskInput>({
+		mutationFn: createTask,
+		onSettled: () => {
+			queryClient.invalidateQueries({
+				queryKey: getTasksKey.single(activeBoard?.id!)
+			});
+		}
 	});
+};
