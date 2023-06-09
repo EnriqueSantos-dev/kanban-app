3164ff8c56c565cb366dffc3547c15d323cac0e1
Enrique Santos <santosenrique869@gmail.com>
2023-06-09 09:38:16 -0300

    chore: delete unused hook

--- src/hooks/useActiveBoard.ts
+++
@@ -1,7 +0,0 @@
-import { useActiveBoardStore } from '~/stores/active-board-store';
-
-export const useActiveBoard = () => {
-	const { activeBoard, setActiveBoard } = useActiveBoardStore();
-
-	return { activeBoard, setActiveBoard };
-};
