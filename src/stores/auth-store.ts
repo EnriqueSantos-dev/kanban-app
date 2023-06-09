1ba22a4ef9a68c95314822ee8910ef508285a280
Enrique Santos <santosenrique869@gmail.com>
2023-06-08 21:26:36 -0300

    refactor: initialize token as undefined

--- src/stores/auth-store.ts
+++ src/stores/auth-store.ts
@@ -1,23 +1,22 @@
 import { create } from 'zustand';
 import { UserProfile } from '~/types';
-import { getAuthToken } from '~/utils/auth';
 
 type AuthState = {
 	user: UserProfile | undefined;
 	token: string | undefined;
 	actions: {
-		setToken: (token: string) => void;
+		setToken: (token?: string) => void;
 		setUser: (user: UserProfile) => void;
 		clearAll: () => void;
 	};
 };
 
 const AuthStore = create<AuthState>((set) => ({
-	token: getAuthToken(),
+	token: undefined,
 	user: undefined,
 
 	actions: {
-		setToken: (token: string) => set({ token }),
+		setToken: (token?: string) => set({ token }),
 		setUser: (user: UserProfile) => set({ user }),
 		clearAll: () => set({ user: undefined, token: undefined })
 	}
