d6a50484337df3a612047ef0c12adcf7f660bab1
Enrique Santos <santosenrique869@gmail.com>
2023-06-09 10:25:12 -0300

    chore: remove import for deleted hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,5 @@
 export * from './useNotificationToasty';
 export * from './useInteractiveForm';
-export * from './useActiveBoard';
 export * from './useCreateBoard';
 export * from './useEditBoard';
 export * from './useSignIn';
