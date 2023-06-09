2e85bb5b567e2aa9489abf927d1dc5655bc8400d
Enrique Santos <santosenrique869@gmail.com>
2023-06-09 09:28:51 -0300

    feat: add new functions

--- src/services/tasks.service.ts
+++ src/services/tasks.service.ts
@@ -1,4 +1,5 @@
 import { api } from '~/lib';
+import { Column, Task } from '~/types';
 
 export type CreateTaskInput = {
 	title: string;
@@ -31,4 +32,50 @@ export async function createTask(
 ): Promise<CreateTaskOutPutDto> {
 	const { data } = await api.post('tasks', input);
 	return data;
+}
+
+export type GetTasks = {
+	tasks: Task[];
+} & Column;
+
+export type GetTasksInputTest = {
+	boardId: string;
+};
+
+export async function getTasksFromBoard({ boardId }: GetTasksInputTest) {
+	const { data } = await api.get<GetTasks[]>(`tasks/${boardId}`);
+
+	return data;
+}
+
+export type DeleteTaskInput = {
+	taskId: string;
+};
+
+export async function deleteTask({ taskId }: DeleteTaskInput): Promise<void> {
+	await api.delete(`tasks/${taskId}/delete`);
+}
+
+export type UpdateSubTaskStatusInput = {
+	subTaskId: string;
+	isDone: boolean;
+};
+
+export async function updateSubTaskStatus({
+	subTaskId,
+	isDone
+}: UpdateSubTaskStatusInput): Promise<void> {
+	await api.patch(`subtasks/${subTaskId}/change-status`, { isDone });
+}
+
+export type MoveTaskInput = {
+	taskId: string;
+	columnId: string;
+};
+
+export async function moveTask({
+	taskId,
+	columnId
+}: MoveTaskInput): Promise<void> {
+	await api.patch(`tasks/${taskId}/move`, { columnId });
 }
