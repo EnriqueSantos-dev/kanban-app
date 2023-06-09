49fc12f0958fac09b60ab6063800a1f145e26374
Enrique Santos <santosenrique869@gmail.com>
2023-06-09 09:46:01 -0300

    feat: create hook useChangeStatusSubTask

---
+++ src/hooks/useChangeStatusSubTask.ts
@@ -0,0 +1,7 @@
+import { useMutation } from '@tanstack/react-query';
+import { updateSubTaskStatus } from '~/services/tasks.service';
+
+export const useChangeStatusSubTask = () =>
+	useMutation({
+		mutationFn: updateSubTaskStatus
+	});
