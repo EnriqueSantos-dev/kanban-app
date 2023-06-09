757db527b921d8818158b2a33cf9e9fc8d699fa7
Enrique Santos <santosenrique869@gmail.com>
2023-06-08 21:25:39 -0300

    refactor: change loading logo img to eager

--- src/shared/components/LoadingPage.tsx
+++ src/shared/components/LoadingPage.tsx
@@ -9,6 +9,7 @@ export function LoadingPage() {
 			<p className="text-mediumGrey flex animate-pulse items-center gap-3 text-3xl font-bold leading-relaxed dark:text-white md:text-5xl">
 				<img
 					src="/assets/logo.svg"
+					loading="eager"
 					alt="Logo"
 					className="h-6 w-6 md:h-10 md:w-10"
 				/>
