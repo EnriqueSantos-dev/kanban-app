94d60cdd30bf600971c544b812b17940f2c491a9
Enrique Santos <santosenrique869@gmail.com>
2023-06-09 09:45:19 -0300

    refactor: remove columns key factory

--- src/utils/query-keys-factories.ts
+++ src/utils/query-keys-factories.ts
@@ -1,8 +1,3 @@
 export const userKeys = {
 	profile: ['user/profile'] as const
 };
-
-export const columnKeys = {
-	column: ['column'] as const,
-	columnId: (id: string) => [...columnKeys.column, id] as const
-};
