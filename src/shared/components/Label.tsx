0d90dc067ab0a43cdd439287814fd3793fc715ef
Enrique Santos <santosenrique869@gmail.com>
2023-06-10 18:04:53 -0300

    style: change font-size to small size

--- src/shared/components/Label.tsx
+++ src/shared/components/Label.tsx
@@ -9,7 +9,7 @@ export function Label({ label, className, children }: LabelTextProps) {
 	return (
 		<label
 			className={cn(
-				'text-mediumGrey flex cursor-pointer flex-col gap-2 text-[13px] font-bold first-letter:capitalize dark:text-white',
+				'text-mediumGrey flex cursor-pointer flex-col gap-2 text-sm font-bold first-letter:capitalize dark:text-white',
 				className
 			)}
 		>
