5312412b05664fe90221977ca2bd0825de67bd31
Enrique Santos <santosenrique869@gmail.com>
2023-06-10 18:06:08 -0300

    refactor: put content component inside overlay component

--- src/shared/components/TaskCard.tsx
+++ src/shared/components/TaskCard.tsx
@@ -146,102 +146,103 @@ export function TaskCard({
 				</DialogTrigger>
 
 				<DialogPortal>
-					<DialogOverlay />
-					<DialogContent>
-						<div className="flex items-center justify-between">
-							<DialogHeader className="mb-0 text-xl">{name}</DialogHeader>
-							<Popover.Root>
-								<Popover.Trigger asChild>
-									<button
-										type="button"
-										aria-label="Edit or delete task"
-										className="group flex h-5 items-center justify-center px-3 outline-none"
+					<DialogOverlay>
+						<DialogContent>
+							<div className="mt-2 flex items-center justify-between md:mt-0">
+								<DialogHeader className="mb-0 text-xl">{name}</DialogHeader>
+								<Popover.Root>
+									<Popover.Trigger asChild>
+										<button
+											type="button"
+											aria-label="Edit or delete task"
+											className="group flex h-5 items-center justify-center px-3 outline-none"
+										>
+											<svg
+												width="5"
+												height="20"
+												xmlns="http://www.w3.org/2000/svg"
+												className="group-focus:fill-purple fill-mediumGrey hover:fill-purple dark:hover:fill-purpleHover cursor-pointer"
+											>
+												<g fillRule="evenodd">
+													<circle cx="2.308" cy="2.308" r="2.308" />
+													<circle cx="2.308" cy="10" r="2.308" />
+													<circle cx="2.308" cy="17.692" r="2.308" />
+												</g>
+											</svg>
+										</button>
+									</Popover.Trigger>
+									<Popover.Content
+										className={cn(
+											'absolute transition-all top-10 rounded-md bg-white p-4 right-0 w-44 shadow-md dark:bg-darkGrey data-[state=open]:animate-fade-in data-[state=open]:animate-scale-up group-data-[state=closed]:animate-fade-out z-50'
+										)}
 									>
-										<svg
-											width="5"
-											height="20"
-											xmlns="http://www.w3.org/2000/svg"
-											className="group-focus:fill-purple fill-mediumGrey hover:fill-purple dark:hover:fill-purpleHover cursor-pointer"
-										>
-											<g fillRule="evenodd">
-												<circle cx="2.308" cy="2.308" r="2.308" />
-												<circle cx="2.308" cy="10" r="2.308" />
-												<circle cx="2.308" cy="17.692" r="2.308" />
-											</g>
-										</svg>
-									</button>
-								</Popover.Trigger>
-								<Popover.Content
-									className={cn(
-										'absolute transition-all top-10 rounded-md bg-white p-4 right-0 w-44 shadow-md dark:bg-darkGrey data-[state=open]:animate-fade-in data-[state=open]:animate-scale-up group-data-[state=closed]:animate-fade-out z-50'
-									)}
-								>
-									<div className="flex flex-col items-start gap-4">
-										<button
-											type="button"
-											className="text-mediumGrey dark:hover:text-lightGrey dark:focus:text-lightGrey cursor-pointer whitespace-nowrap text-sm font-medium capitalize outline-none transition-colors hover:text-black focus:text-black"
-											onClick={handleChangeModalEditTask}
-										>
-											Edit task
-										</button>
+										<div className="flex flex-col items-start gap-4">
+											<button
+												type="button"
+												className="text-mediumGrey dark:hover:text-lightGrey dark:focus:text-lightGrey cursor-pointer whitespace-nowrap text-sm font-medium capitalize outline-none transition-colors hover:text-black focus:text-black"
+												onClick={handleChangeModalEditTask}
+											>
+												Edit task
+											</button>
 
-										<button
-											type="button"
-											className="text-red dark:hover:text-redHover dark:focus:text-redHover cursor-pointer whitespace-nowrap text-sm font-medium capitalize outline-none transition-colors hover:text-[#BC2727] focus:text-[#BC2727]"
-											onClick={handleChangeModalDelete}
-										>
-											Delete Task
-										</button>
-									</div>
-								</Popover.Content>
-							</Popover.Root>
-						</div>
+											<button
+												type="button"
+												className="text-red dark:hover:text-redHover dark:focus:text-redHover cursor-pointer whitespace-nowrap text-sm font-medium capitalize outline-none transition-colors hover:text-[#BC2727] focus:text-[#BC2727]"
+												onClick={handleChangeModalDelete}
+											>
+												Delete Task
+											</button>
+										</div>
+									</Popover.Content>
+								</Popover.Root>
+							</div>
 
-						{description && (
-							<DialogDescription className="text-mediumGrey mt-8 line-clamp-3 font-medium">
-								{description}
-							</DialogDescription>
-						)}
-
-						<div className="mt-5 space-y-3">
-							<p className="text-mediumGrey font-bold dark:text-white">
-								SubTasks&nbsp;
-								{subtasks.length > 0
-									? `(${completedSubtasksCount} of ${subtasks.length})`
-									: null}
-							</p>
-
-							{subtasks?.length === 0 && (
-								<p className="text-mediumGrey text-sm font-medium">
-									No subtasks yet
-								</p>
+							{description && (
+								<DialogDescription className="text-mediumGrey mt-8 line-clamp-3 font-medium">
+									{description}
+								</DialogDescription>
 							)}
-							<ul className="flex flex-col gap-3">
-								{subtasks?.map((subtask) => (
-									<ToggleSubTaskCard
-										key={subtask.id}
-										{...subtask}
-										isChecked={getIfTaskIsDone(checkedSubtasks, subtask.id)}
-										onChangeStatus={({ subTaskId, isDone }) => {
-											handleChangeCheckedSubtask(subTaskId);
-											mutationChangeStatusSubTask.mutate({
-												isDone,
-												subTaskId
-											});
-										}}
-									/>
-								))}
-							</ul>
-						</div>
 
-						<Label label="Status" className="mt-4 text-base">
-							<SelectStatusTask
-								options={statusOptions}
-								defaultOption={defaultStatus}
-								onValueChange={handleMoveTask}
-							/>
-						</Label>
-					</DialogContent>
+							<div className="mt-5 space-y-3">
+								<p className="text-mediumGrey text-2xs font-bold dark:text-white">
+									SubTasks&nbsp;
+									{subtasks.length > 0
+										? `(${completedSubtasksCount} of ${subtasks.length})`
+										: null}
+								</p>
+
+								{subtasks?.length === 0 && (
+									<p className="text-mediumGrey text-sm font-medium">
+										No subtasks yet
+									</p>
+								)}
+								<ul className="flex flex-col gap-3">
+									{subtasks?.map((subtask) => (
+										<ToggleSubTaskCard
+											key={subtask.id}
+											{...subtask}
+											isChecked={getIfTaskIsDone(checkedSubtasks, subtask.id)}
+											onChangeStatus={({ subTaskId, isDone }) => {
+												handleChangeCheckedSubtask(subTaskId);
+												mutationChangeStatusSubTask.mutate({
+													isDone,
+													subTaskId
+												});
+											}}
+										/>
+									))}
+								</ul>
+							</div>
+
+							<Label label="Status" className="mt-4 text-sm">
+								<SelectStatusTask
+									options={statusOptions}
+									defaultOption={defaultStatus}
+									onValueChange={handleMoveTask}
+								/>
+							</Label>
+						</DialogContent>
+					</DialogOverlay>
 				</DialogPortal>
 			</Dialog>
 
