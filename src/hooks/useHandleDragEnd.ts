ce8291c5ca66b30c9951296e559d8ae45bda3025
Enrique Santos <santosenrique869@gmail.com>
2023-06-10 10:18:09 -0300

    feat: create hook useHandleDragEnd

---
+++ src/hooks/useHandleDragEnd.ts
@@ -0,0 +1,49 @@
+import { DragEndEvent } from '@dnd-kit/core';
+import { TaskCardCurrent } from '~/shared/components/TaskCard';
+import { BoardType } from '~/stores/active-board-store';
+import { useMoveTask } from './useMoveTask';
+import { useUpdateOrder } from './useUpdateOrder';
+
+export function useHandleDragEnd({ activeBoard }: { activeBoard: BoardType }) {
+	const mutationMoveTask = useMoveTask();
+	const mutationUpdateOrder = useUpdateOrder({ activeBoard });
+
+	const handleDragEnd = (e: DragEndEvent) => {
+		const activeElement = e.active;
+		const overElement = e.over;
+
+		if (activeElement.id === overElement?.id || !overElement) return;
+
+		const activeElementData = activeElement.data.current as TaskCardCurrent;
+		const overElementData = overElement.data?.current as TaskCardCurrent;
+
+		// when the task is moved to another column and over task is undefined
+		if (activeElementData.columnId !== overElement.id && !overElementData) {
+			mutationMoveTask.mutate({
+				taskId: activeElement.id as string,
+				columnId: overElement.id as string
+			});
+
+			return;
+		}
+
+		if (activeElementData.columnId === overElementData?.columnId) {
+			mutationUpdateOrder.mutate({
+				taskId: activeElement.id as string,
+				destinationColumnId: activeElementData.columnId,
+				newOrder: overElementData?.order,
+				index: overElementData.sortable.index
+			});
+			return;
+		}
+
+		mutationUpdateOrder.mutate({
+			taskId: activeElement.id as string,
+			destinationColumnId: overElementData.columnId,
+			newOrder: overElementData?.order,
+			index: overElementData.sortable.index
+		});
+	};
+
+	return { handleDragEnd };
+}
