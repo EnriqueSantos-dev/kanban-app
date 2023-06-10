e3f15c17eb51db1b7b1a877fc0d8a542fe48f55a
Enrique Santos <santosenrique869@gmail.com>
2023-06-10 09:46:21 -0300

    refactor: add logging in queries

--- src/database/prisma.service.ts
+++ src/database/prisma.service.ts
@@ -3,6 +3,10 @@ import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+	constructor() {
+		super({ log: ['query'] });
+	}
+
 	async onModuleInit() {
 		await this.$connect();
 	}
