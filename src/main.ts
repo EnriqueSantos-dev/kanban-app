eb4aa4e232e7cbd5470a86551cd71ed29c4427f7
Enrique Santos <santosenrique869@gmail.com>
2023-06-12 13:37:14 -0300

    refactor: change sameSite to none

--- src/main.ts
+++ src/main.ts
@@ -7,18 +7,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	const config = app.get(ConfigService);
-	app.enableCors({
-		credentials: true,
-		origin: config.get('CLIENT_URL'),
-		exposedHeaders: ['Set-Cookie']
-	});
 
 	app.use(cookieParser());
-	app.useGlobalPipes(
-		new ValidationPipe({
-			transform: true
-		})
-	);
+	app.enableCors({ credentials: true, origin: config.get('CLIENT_URL') });
+	app.useGlobalPipes(new ValidationPipe({ transform: true }));
 	app.setGlobalPrefix('api/v1');
 
 	await app.listen(config.get('PORT'));
