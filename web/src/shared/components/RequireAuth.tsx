7330d0f416a118d2dbf0cafdf7af084fa071b462
Enrique Santos <santosenrique869@gmail.com>
2023-09-08 18:27:00 -0300

    feat: create RequireAuth HOC component

---
+++ web/src/shared/components/RequireAuth.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from 'react';
+import { Navigate } from 'react-router-dom';
+
+import { useGetProfileQuery } from '~/hooks';
+
+import { LoadingPage } from '~/shared/components/LoadingPage';
+
+import { useAuthStore } from '~/stores/auth-store';
+
+export function RequireAuth({ children }: { children: ReactNode }) {
+	const query = useGetProfileQuery();
+	const { isAuth, token } = useAuthStore();
+
+	if (!token) {
+		return <Navigate to="/auth/login" />;
+	}
+
+	if (query.isLoading) {
+		return <LoadingPage />;
+	}
+
+	if (!isAuth || query.isError) {
+		return <Navigate to="/auth/login" />;
+	}
+
+	return <>{children}</>;
+}
